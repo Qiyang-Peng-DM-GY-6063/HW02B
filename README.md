0d3ff88129ff46a859de3c325430e31bb1735ca7
potuitou <qpeng@risd.edu>
2024-09-22 17:29:58 -0400

    motion with color change & size change

--- README.md
+++ README.md
@@ -1,60 +1,10 @@
-# p5.js Template
+# HW02A/B
+## HW02B - Variations based on Vera Molnar's work by p5.js
 
-This is a README file that can be used to describe and document your assignment.
+Credit: Vera Molnar
 
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+[Original Art Piece Introduction](https://www.artnet.com/artists/vera-molnar/histoire-di-history-of-i-a-bhiz2iTdxFQ6SoJZJFdyxQ2)
 
----
----
+Art Piece Image:  
+![History of I](./History%20of%20I.jpg)
 
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
-
-**bold text**
-
-*italicized text*
-
-~~strikethrough text~~
-
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,121 @@
+let newCanvas;
+let backgroundColor;
+let horizontalColor;
+let verticalColor;
+let bSwitch = 1;
+let wSwitch = -1;
+let hsSwitch = 1;
+let vsSwitch = -1;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  newCanvas = createCanvas(525, 525);
+  newCanvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+  colorMode(RGB, 255);
+
+  b = 0;
+  w = 255;
+  a = 45;
+  hs = 90;
+  vs = 90;
+
+  backgroundColor = color(252, 241, 226);
+
+  background(backgroundColor);
+
+  noStroke();
+
+  verticalColor = color(w, w, w, 45);
+  horizontalColor = color(b, b, b, 45);
+
+  drawH(hs, vs);
+
+  frameRate(20);
 }
 
 function draw() {
+  //color
+
+  b += bSwitch;
+  w += wSwitch;
+
+  if (b >= 255 || b <= 0) {
+    bSwitch *= -1;
+  }
+
+  if (w >= 255 || w <= 0) {
+    wSwitch *= -1;
+  }
+
+  //size
+
+  hs += hsSwitch;
+  vs += vsSwitch;
+
+  if (hs >= 105 || hs <= 75) {
+    hsSwitch *= -1;
+  }
+
+  if (vs >= 105 || vs <= 75) {
+    vsSwitch *= -1;
+  }
+
+  verticalColor = color(w, w, w, a);
+  horizontalColor = color(b, b, b, a);
+
+  drawH(hs, vs);
 }
+
+function drawH(hs, vs) {
+  //Line 1
+  verticalShape(30, 30, vs, verticalColor);
+  horizontalShape(120, 30, hs, horizontalColor);
+  verticalShape(210, 30, vs, verticalColor);
+  horizontalShape(300, 30, hs, horizontalColor);
+  verticalShape(390, 30, vs, verticalColor);
+
+  //Line 2
+  horizontalShape(30, 120, hs, horizontalColor);
+  verticalShape(120, 120, vs, verticalColor);
+  horizontalShape(210, 120, hs, horizontalColor);
+  verticalShape(300, 120, vs, verticalColor);
+  horizontalShape(390, 120, hs, horizontalColor);
+
+  //Line 3
+  verticalShape(30, 210, vs, verticalColor);
+  horizontalShape(120, 210, hs, horizontalColor);
+  verticalShape(210, 210, vs, verticalColor);
+  horizontalShape(300, 210, hs, horizontalColor);
+  verticalShape(390, 210, vs, verticalColor);
+
+  //Line 4
+  horizontalShape(30, 300, hs, horizontalColor);
+  verticalShape(120, 300, vs, verticalColor);
+  horizontalShape(210, 300, hs, horizontalColor);
+  verticalShape(300, 300, vs, verticalColor);
+  horizontalShape(390, 300, hs, horizontalColor);
+
+  //Line 5
+  verticalShape(30, 390, vs, verticalColor);
+  horizontalShape(120, 390, hs, horizontalColor);
+  verticalShape(210, 390, vs, verticalColor);
+  horizontalShape(300, 390, hs, horizontalColor);
+  verticalShape(390, 390, vs, verticalColor);
+}
+
+function verticalShape(x, y, s, color) {
+  fill(color);
+  square(x, y, s);
+  fill(backgroundColor);
+  square(x + s / 3, y, s / 3);
+  square(x + s / 3, y + (s * 2) / 3, s / 3);
+}
+
+function horizontalShape(x, y, s, color) {
+  fill(color);
+  square(x, y, s);
+  fill(backgroundColor);
+  square(x, y + s / 3, s / 3);
+  square(x + (s * 2) / 3, y + s / 3, s / 3);
+}
+
+//credit artist: Vera Molnar
